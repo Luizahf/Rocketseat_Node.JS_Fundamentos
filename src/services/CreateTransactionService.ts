6e8df171b85c4b7e905ade96967e824461a9df6e
Luíza Freire <luiza.freire@localiza.com>
2020-12-03 20:06:14 -0300

    Creating and listing transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type } : Request): Transaction {
+
+    var balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new Error("Invalid value.");
+    }
+
+    var transaction= this.transactionsRepository.create({ title, value, type});
+    return transaction;
   }
 }
 
